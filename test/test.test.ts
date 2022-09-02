7f9e28c267b52d2da6d3038bd908e8d99e31e5f8
matthew.crompton <matthew.crompton@banxware.com>
2022-09-02 14:52:58 +0200

    MAtt

--- test/test.test.ts
+++ test/test.test.ts
@@ -1,3 +1,35 @@
 test('dummy test', () => {
-  expect(1 + 2).toBe(3)
+  const parseString = (): string => {}
+
+  it('parses the string', () => {
+    type direction = 'N' | 'S' | 'E' | 'W'
+    type move = 'L' | 'R' | 'M'
+    type moveSet = move[]
+    type point = { x: number, y: number} 
+    type parsedData = 
+
+    const input: string = `5 5
+    1 2 N
+    LMLMLMLMM
+    3 3 E
+    MMRMMRMRRM`
+
+    const output = {
+      sizeOfMap: { x: 5, y: 5 },
+      rovers: [
+        {
+          location: { x: 1, y: 1 },
+          direction: 'N',
+          moveSet: ['L', 'M', 'L', 'M', 'L', 'M', 'L', 'M', 'M'],
+        },
+        {
+          location: { x: 3, y: 3 },
+          direction: 'E',
+          moveSet: ['M', 'M', 'R', 'M', 'M', 'R', 'M', 'R', 'R', 'M'],
+        },
+      ],
+    }
+
+    expect(parseString(input)).toBe(output)
+  })
 })
